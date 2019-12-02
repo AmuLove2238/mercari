5dc37110164f7fc7fec213f1b9006539ca239d70
kentatsuchi <ken.sb.bb@gmail.com>
2019-12-02 14:37:42 +0900

    registration user 修正７
    ルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,11 @@ Rails.application.routes.draw do
   get 'exhibit/index'=>"exhibit#index"
   resources :items
 
-  get "signup", to: "signup#index"
   resources :signup do
     collection do
       get 'first_signup_page'   #最初の登録ページ
       post 'sms_confirmation'  #電話番号登録
       post 'address'  #送付先登録
-      post 'credit_card'  #入力が全て完了
       get 'complete_signup'  #登録完了後
     end
   end
