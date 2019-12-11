10428ef69585102eed1b261646186589af563ae5
hot-and-cool <hisato0217take@gmail.com>
2019-12-12 00:20:35 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :move_to_index, except: :index
   
-  before_action :set_item,only: [:show,:destroy]
+  before_action :set_item,only: [:show,:destroy,:edit,:update]
 
   def index
     @items = Item.order("id DESC").limit(10)
