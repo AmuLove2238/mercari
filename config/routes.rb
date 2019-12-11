85a054fb39f902053152ac75dacd39a7159a8034
hanchan810 <tec4pronb0y@hotmail.com>
2019-12-11 18:25:33 +0900

    ユーザーマイページ２

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'purchase/index'
+
+  get 'purchase/done'
+
   root to: 'items#index'
 
-  resources :items do
-    resources :images
-  end
+  resources :items 
   
   devise_for :users, controllers: {
   confirmations: 'users/confirmations',
@@ -13,35 +15,46 @@ Rails.application.routes.draw do
 }
   
 
-  get 'exhibit/index'=>"exhibit#index"
 
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show] 
 
-  resources :signup, only: [:index,:create] do
+
+  resources :signup, only: [:index, :create] do
     collection do
-      get 'first_signup_page'   #最初の登録ページ
-      post 'sms_confirmation'  #電話番号登録
-      post 'address'  #送付先登録
-      get 'complete_signup'  #登録完了後
+      get 'first_signup_page'     #最初の登録ページ
+      post 'sms_confirmation'     #電話番号登録ページ
+      post 'address'              #住所先登録ページ
+      get 'credit_card'           #クレジットカード登録ページ
+      get 'complete_signup'       #登録完了ページ
     end
   end
 
-  resources :card, only: [:new, :show, :create] do
+  resources :card, only: [:index, :new, :show, :create] do
     collection do
-      post 'card',  to: 'card#new'
       post 'show', to: 'card#show'
+      post 'pay_registration', to: 'card#pay_registration'
       post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#delete'
+      delete 'delete', to: 'card#delete'
     end
   end
 
-  resources :users do [:show]
+  resources :mypage do [:index]
     collection do
       get 'mypage'
-      get 'profile'
-      get 'logout'
-      get 'user_mypage'
+      get 'identificatio'
+      get "purchase_screen"
+      get 'information'     #本人情報 後で追加
+      get 'profile'         #プロフィール
+      get 'logout'          #ログアウト
     end
   end
 
+
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index'           #商品購入確認ページ
+      post 'pay'            #電話番号登録ページ
+      get 'done'            #購入完了ページ
+    end
+  end
 end
